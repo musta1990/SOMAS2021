157ef60f653bba773dbdb81f7bdcf5a9b14d4469
musta1990 <alisabamustafa@gmail.com>
2021-02-12 20:27:42 -0600

    Cambios en sección de información

--- informacion_general.php
+++ informacion_general.php
@@ -3,20 +3,146 @@ include("header.php");
 ?>
 
 <div class="container-fluid">
-    <h>INFORMACIÓN GENERAL</h>
-    <div class="row">
-    <div class="col-md-3">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor interdum leo. Aenean finibus diam mi, a porta elit lacinia a. Donec vitae bibendum justo, pharetra sodales neque. Phasellus lorem felis, laoreet et viverra accumsan, luctus at felis. Nullam vitae venenatis sapien. Nam vestibulum urna non pellentesque porta. Pellentesque ornare nisl quam, ac elementum arcu volutpat sit amet. Maecenas vehicula bibendum justo a tempor. Ut vel tincidunt tortor, at euismod mi. Curabitur molestie odio vitae lorem tristique venenatis. Suspendisse potenti. Nulla vitae porttitor sapien, et consequat arcu. Vivamus eu risus lectus. Sed sem sapien, hendrerit posuere sollicitudin vel, lacinia ac ante. Phasellus ex augue, ultricies eu hendrerit sed, condimentum ut arcu. In commodo faucibus pellentesque.
-    </div>
-    <div class="col-md-4">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor interdum leo. Aenean finibus diam mi, a porta elit lacinia a. Donec vitae bibendum justo, pharetra sodales neque. Phasellus lorem felis, laoreet et viverra accumsan, luctus at felis. Nullam vitae venenatis sapien. Nam vestibulum urna non pellentesque porta. Pellentesque ornare nisl quam, ac elementum arcu volutpat sit amet. Maecenas vehicula bibendum justo a tempor. Ut vel tincidunt tortor, at euismod mi. Curabitur molestie odio vitae lorem tristique venenatis. Suspendisse potenti. Nulla vitae porttitor sapien, et consequat arcu. Vivamus eu risus lectus. Sed sem sapien, hendrerit posuere sollicitudin vel, lacinia ac ante. Phasellus ex augue, ultricies eu hendrerit sed, condimentum ut arcu. In commodo faucibus pellentesque. 
-    </div>
-    <div class="col-md-5">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor interdum leo. Aenean finibus diam mi, a porta elit lacinia a. Donec vitae bibendum justo, pharetra sodales neque. Phasellus lorem felis, laoreet et viverra accumsan, luctus at felis. Nullam vitae venenatis sapien. Nam vestibulum urna non pellentesque porta. Pellentesque ornare nisl quam, ac elementum arcu volutpat sit amet. Maecenas vehicula bibendum justo a tempor. Ut vel tincidunt tortor, at euismod mi. Curabitur molestie odio vitae lorem tristique venenatis. Suspendisse potenti. Nulla vitae porttitor sapien, et consequat arcu. Vivamus eu risus lectus. Sed sem sapien, hendrerit posuere sollicitudin vel, lacinia ac ante. Phasellus ex augue, ultricies eu hendrerit sed, condimentum ut arcu. In commodo faucibus pellentesque. 
+<!-- INFORMACIÓN GENERAL -->
+<div class="panel panel-default">
+  <div class="panel-heading" style="background-color:green;">
+    <h1 class="panel-title" style="font-size:23px; font-variant:small-caps; font-weight:bold; color:white; text-align:center;">Información General</h1>
+  </div>
+  <div class="panel-body">
+  <div class="row">
+        <div class="col-md-3"><br><br>
+          <div class="panel-body" style="background-color:#ccffcc;">
+            <p style="font-weight:bold; font-size:15px;">Renovación de vigencia o nueva membresía de la SOMAS, A. C.</p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="panel-heading">
+            <h3 class="panel-title" style="font-variant:small-caps; font-size:19px; text-align:center;">Cuotas de Recuperación</h3>
+          </div>
+          <div class="panel-body">
+            <div class="row">
+              <div class="col-xs-8 col-sm-6">
+                <p style="font-size:15px;">Hasta el 30 de junio de 2021</p>
+              </div>
+              <div class="col-xs-4 col-sm-6">
+                <p style="font-size:15px;">A partir del 1 de julio de 2021</p>
+              </div>
+            </div>
+          </div>
+          <div class="panel-footer" style="background-color:#ccffcc;">
+            <div class="row">
+              <div class="col-md-4">
+                <p style="font-size:15px;">$ 500.00</p>
+              </div>
+              <div class="col-md-4">
+                <p style="font-size:15px;">$ 500.00</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-5">
+          <h3 class="panel-title" style="font-variant:small-caps; font-size:19px; text-align:center;">Incluye</h3><br>
+            <ul class="list-group">
+              <li class="list-group-item"><p style="font-size:15px;">&bull; Constancia de membresía con vigencia de enero a diciembre por dos años consecutivos</p></li>
+              <li class="list-group-item"><p style="font-size:15px;">&bull; Acceso a la colección digital de publicaciones de la SOMAS</p></li>
+              <li class="list-group-item"><p style="font-size:15px;">&bull; Acceso al Seminario Virtual mensual de la SOMAS</p></li>
+              <li class="list-group-item"><p style="font-size:15px;">&bull; Descuento en inscripción al congreso y en los cursos pre-congreso</p></li>
+              <li class="list-group-item"><p style="font-size:15px;">&bull; Participación en la asamblea ordinaria general de la SOMAS, A. C.</p></li>
+              <li class="list-group-item"><p style="font-size:15px;">&bull; Derecho de voto en la etapa final del concurso de fotografía</p></i>
+            </ul>
+        </div>
     </div>
   </div>
+</div>
+<!-- CUOTAS DE RECUPERACIÓN -->
+<div class="panel panel-default">
+  <div class="panel-body" style="background-color:#003d99;">
+  <div class="row">
+        <div class="col-md-4">
+          <div class="panel-heading">
+            <h3 class="panel-title" style="font-variant:small-caps; font-size:19px; color:white; text-align:center;">Inscripción</h3>
+          </div>
+          <div class="panel-body" style="background-color:#f2f2f2;">
+            <p style="font-size:15px;">Al XVI Simposio Internacional de Agricultura Sostenible,  
+XI Congreso Nacional de Agricultura Sostenible, y los eventos paralelos:</p>
+            <p>&bull; XVII Simposio Nacional sobre Sustancias Vegetales y Minerales en el Combate de Plagas,</p>
+            <p>&bull; IX Coloquio sobre Agroecosistemas y Sustentabilidad,</p>
+            <p>&bull; V Simposio sobre Recursos Genéticos, y</p>
+            <p>&bull; VIII Concurso de Tesis en Agricultura Sostenible.</p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="panel-heading">
+            <h3 class="panel-title" style="font-variant:small-caps; font-size:18px; color:white; text-align:center;">Cuotas de Recuperación</h3>
+          </div>
+          <div class="panel-body">
+            <div class="row">
+              <div class="col-lg-6">
+                <p>Hasta el 30 de junio de 2021</p>
+              </div>
+              <div class="col-lg-6">
+                <p>A partir del 1 de julio de 2021</p>
+              </div>
+            </div><br>
+            <div class="row">
+              <div class="col-lg-6">
+                <p style="font-weight:bold;text-align:center;">PROFESIONISTAS SOCIOS</p>
+                <p style="text-align:center;">Gratuita con membresía</p>
+              </div>
+              <div class="col-lg-6">
+                <p style="text-align:center;">$ 150.00</p>
+              </div>
+            </div><br>
+            <div class="row">
+              <div class="col-lg-6">
+                <p style="font-weight:bold;text-align:center;">PROFESIONISTAS NO SOCIOS</p>
+                <p style="text-align:center;">$ 650.00</p>
+              </div>
+              <div class="col-lg-6">
+                <p style="text-align:center;">$ 750.00</p>
+              </div>
+            </div><br>
+            <div class="row">
+              <div class="col-lg-6">
+                <p style="font-weight:bold;text-align:center;">ESTUDIANTES*</p>
+                <p style="text-align:center;">$ 250.00</p>
+              </div>
+              <div class="col-lg-6">
+                <p style="text-align:center;">$ 350.00</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <h3 class="panel-title" style="font-variant:small-caps; font-size:19px; color:white; text-align:center;">Incluye</h3><br>
+            <ul class="list-group">
+              <li class="list2 list-group-item"><p style="font-size:15px;">&bull; Memorias del Congreso y materiales del evento.</p></li>
+              <li class="list2 list-group-item"><p style="font-size:15px;">&bull; Entrada con derecho de interacción con ponentes del Simposio Internacional, Congreso Nacional y los eventos paralelos.</p></li>
+              <li class="list2 list-group-item"><p style="font-size:15px;">&bull; Derecho de envío de trabajos para presentar hasta dos ponencias como autor responsable en el Congreso.</p></li>
+              <li class="list2 list-group-item"><p style="font-size:15px;">&bull; Constancia de asistencia, y en su caso como ponente.</p></li>
+              <li class="list2 list-group-item"><p style="font-size:15px;">&bull; Entrada a la exposición del  I Concurso de Fotografía Digital en Agricultura Sostenible, y a la I Expo Virtual de Experiencias Exitosas de Agricultura Sostenible, así como a los eventos culturales.</p></li>
+            </ul>
+        </div>
+    </div>
+    <div class="panel-footer" style="background-color:#f2f2f2;">
+      <p>*Para inscripciones grupales (de 20 o más estudiantes) dirigirse a tesorera@somas.org.mx para descuentos adicionales.</p>
+    </div>
+  </div>
+</div>
 </div>
 
 <?php
 include("footer.php");
 ?>
+
+<style>
+.list-group-item {
+  background-color:#ccffcc;
+}
+.list2 {
+  background-color:#f2f2f2;
+}
+.col-lg-6 {
+  background-color:#f2f2f2;
+}
+</style>
