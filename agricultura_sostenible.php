5c064d88686fa06c35e136ecfe6f9fb816111a4a
vidavidal08 <vidavidal08@gmail.com>
2021-06-04 22:07:58 -0500

    se agregaron botones

--- agricultura_sostenible.php
+++ agricultura_sostenible.php
@@ -1,10 +1,59 @@
 <?php
 include("header.php");
 ?>
+
 <center>
 <img src="img/agricultura1.PNG" alt="Emblema" style="">
 <img src="img/agricultura2.PNG" alt="Emblema" style="">
 </center>
+
+<style type="text/css">
+    .button {
+    background:	#1CAF2A;
+    color:#fff;
+    display:block;
+    font-size: 1.25em;
+    margin:40px;
+        padding:10px 0px;
+    text-align: center;
+        width:200px;
+        text-decoration: none;
+        box-shadow: 0px 3px 0px;
+        border-radius: 50px;
+    }
+    
+    .container1{
+        position:fixed;
+        bottom:20px;
+        right: 150px;
+        
+        
+    } 
+    
+    
+  
+}
+
+    
+</style>
+
+<body>
+
+
+    <div class="container1">
+
+      
+    <a href="pdfs/CONVOCATORIAEVENTOSOMAS2021.pdf" download="Registro" class="button"> Registro manuscrito </a>
+    <a href="pdfs/seccion_congreso_nacional.pdf" download="Ponentes" class="button"> Indicaciones Ponentes</a>
+     <a href="pdfs/seccion_expo_virtual.pdf" download="Seccion extenso" class="button"> Ejemplo extenso</a>
+     <a href="pdfs/seccion_SIMPOSIO.pdf" downlad="seccion resumen" class="button"> Ejemplo resumen </a>
+
+    
+     </div>
+  
+</body>
+   
+
 <?php
 include("footer.php");
 ?>
